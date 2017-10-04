333d44acf6fb843383591b06132f9a0b9e137ce6
anjijava16 <anjijava16@gmail.com>
2017-10-04 20:11:49 +0530

    commited

---
+++ SumOperation/src/com/exadata/iwinner/sum/SumOperationWayFive.java
@@ -0,0 +1,9 @@
+package com.exadata.iwinner.sum;
+
+public class SumOperationWayFive {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Working ...");
+	}
+}
---
+++ SumOperation/src/com/exadata/iwinner/sum/WelcomeMamatha.java
@@ -0,0 +1,8 @@
+package com.exadata.iwinner.sum;
+
+public class WelcomeMamatha {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Mamatha");
+	}
+}
